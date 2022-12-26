f64e4c5dc8496cf05c1b0059fbbaf6cea8c63540
Максим Лесков <9185960@mail.ru>
2022-12-26 18:23:41 +0700

    Task66 commit

---
+++ Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sumFromMToN = 0;
+            for (; m <= n; m++)
+            {
+                sumFromMToN += m;
+            }
+            Console.WriteLine(sumFromMToN);
+            Console.Read();
+        }
+    }
+}
